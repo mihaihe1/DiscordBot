8029de9536c0592d4e1e64f8f1bad24dd50c503d
mihaihe1 <hernest001@gmail.com>
2021-03-06 17:36:01 +0200

    json aff+nasa rover

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import discord
 import os
 import hiddentoken
+import requests
+import json
+import random
+
+def get_affirmation():
+    response = requests.get('https://www.affirmations.dev/')
+    json_data = json.loads(response.text)
+    return json_data['affirmation']
+
+def get_random_image_nasa():
+    rand_sol = random.randint(1, 1000)
+    ok = 0
+    while ok == 0:
+        rand_sol = random.randint(1, 1000)
+        url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol='\
+              +str(rand_sol)+'&camera=fhaz&api_key='+hiddentoken.nasa_api_key
+        response = requests.get(url)
+        #print(response)
+        json_data = json.loads(response.text)
+        #print(json_data)
+        photos = json_data['photos']
+        #print(photos)
+        if photos is not []:
+            #print(response)
+            return photos[0]['img_src']
+
+
+
+    #print(photos[0]['img_src'])
+
+#print(get_random_image_nasa())
+
 client = discord.Client()
 
 @client.event
@@ -14,6 +46,13 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$affirmation'):
+        await message.channel.send(get_affirmation())
+
+    if message.content.startswith('$img'):
+        await message.channel.send(get_random_image_nasa())
+
 
 
 client.run(hiddentoken.token)
